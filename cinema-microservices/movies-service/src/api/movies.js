3b1c34127a53b54411e57a81ee8b50ff15c5c32e
Luiz Duarte <luizfduartejr@gmail.com>
2021-03-27 15:07:01 -0300

    Update movies.js

--- cinema-microservices/movies-service/src/api/movies.js
+++ cinema-microservices/movies-service/src/api/movies.js
@@ -1,11 +1,5 @@
 //movies.js
 module.exports = (app, repository) => {
-    app.get('/movies', async (req, res, next) => {
-        const movies = await repository.getAllMovies();
-        if (!movies || movies.length === 0) return res.sendStatus(404);
-        res.json(movies);
-    })
-
     app.get('/movies/premieres', async (req, res, next) => {
         const movies = await repository.getMoviePremieres();
         if (!movies || movies.length === 0) return res.sendStatus(404);
@@ -16,5 +10,11 @@ module.exports = (app, repository) => {
         const movie = await repository.getMovieById(req.params.id);
         if (!movie) return res.sendStatus(404);
         res.json(movie);
+    })
+    
+    app.get('/movies', async (req, res, next) => {
+        const movies = await repository.getAllMovies();
+        if (!movies || movies.length === 0) return res.sendStatus(404);
+        res.json(movies);
     })
 }
