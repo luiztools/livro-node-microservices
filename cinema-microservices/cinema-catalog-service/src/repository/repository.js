19ea2114efbd315fc158c3cf8f7257efe7e24d23
Luiz Duarte <luizfduartejr@gmail.com>
2021-03-30 15:31:12 -0300

    Update repository.js

--- cinema-microservices/cinema-catalog-service/src/repository/repository.js
+++ cinema-microservices/cinema-catalog-service/src/repository/repository.js
@@ -4,13 +4,13 @@ const { ObjectId } = require("mongodb");
 
 async function getAllCities() {
     const db = await database.connect();
-    return db.collection("cinemaCatalog").find({}, { cidade: 1, uf: 1, pais: 1 }).toArray();
+    return db.collection("cinemaCatalog").find({}).projection({ cidade: 1, uf: 1, pais: 1 }).toArray();
 }
 
 async function getCinemasByCityId(cityId) {
     const objCityId = ObjectId(cityId);
     const db = await database.connect();
-    const cities = await db.collection("cinemaCatalog").find({ _id: objCityId }, { cinemas: 1 }).toArray();
+    const cities = await db.collection("cinemaCatalog").find({ _id: objCityId }).projection({ cinemas: 1 }).toArray();
     return cities[0].cinemas;
 }
 
