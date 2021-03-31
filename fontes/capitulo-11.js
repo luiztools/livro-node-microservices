d48a17e875d1ac36000d916f2c8eae9387ebb354
Luiz Duarte <luizfduartejr@gmail.com>
2021-03-31 12:08:23 -0300

    Update capitulo-11.js

--- fontes/capitulo-11.js
+++ fontes/capitulo-11.js
@@ -180,8 +180,8 @@ async function getAllCities() {
 async function getCinemasByCityId(cityId) {
     const objCityId = new ObjectId(cityId);
     const db = await database.connect();
-    const cities = await db.collection("cinemaCatalog").find({ _id: objCityId }).projection({ cinemas: 1 }).toArray();
-    return cities[0].cinemas;
+    const city = await db.collection("cinemaCatalog").findOne({ _id: objCityId }, {projection: { cinemas: 1 }});
+    return city.cinemas;
 }
 
 async function disconnect() {
